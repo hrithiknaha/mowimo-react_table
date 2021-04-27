25fd4fd7ea13c1c9e1245839f909701abc81b0f4
Hrithik Naha <naha.hrithkk@gmail.com>
2021-04-27 17:11:46 +0530

    Add Filtering

--- src/components/BasicTable.js
+++ src/components/BasicTable.js
@@ -1,8 +1,9 @@
 import React, { useMemo } from "react";
 
-import { useTable, useSortBy } from "react-table";
+import { useTable, useSortBy, useGlobalFilter } from "react-table";
 import { COLUMNS } from "../config/columns";
 import "./BasicTable.css";
+import GlobalFilter from "./GlobalFilter";
 
 function BasicTable({ ROWS }) {
 	const columns = useMemo(() => COLUMNS, []);
@@ -14,43 +15,53 @@ function BasicTable({ ROWS }) {
 		headerGroups,
 		rows,
 		prepareRow,
+		state,
+		setGlobalFilter,
 	} = useTable(
 		{
 			columns,
 			data,
 		},
+		useGlobalFilter,
 		useSortBy
 	);
 
+	const { globalFilter } = state;
+
 	return (
-		<table {...getTableProps()}>
-			<thead>
-				{headerGroups.map((headerGroup) => (
-					<tr {...headerGroup.getHeaderGroupProps()}>
-						{headerGroup.headers.map((column) => (
-							<th {...column.getHeaderProps(column.getSortByToggleProps())}>
-								{column.render("Header")}{" "}
-								<span>
-									{column.isSorted ? (column.isSortedDesc ? "⬇️" : "⬆️") : ""}
-								</span>
-							</th>
-						))}
-					</tr>
-				))}
-			</thead>
-			<tbody {...getTableBodyProps()}>
-				{rows.map((row) => {
-					prepareRow(row);
-					return (
-						<tr {...row.getRowProps()}>
-							{row.cells.map((cell) => {
-								return <td {...cell.getCellProps()}>{cell.render("Cell")}</td>;
-							})}
+		<>
+			<GlobalFilter filter={globalFilter} setFilter={setGlobalFilter} />
+			<table {...getTableProps()}>
+				<thead>
+					{headerGroups.map((headerGroup) => (
+						<tr {...headerGroup.getHeaderGroupProps()}>
+							{headerGroup.headers.map((column) => (
+								<th {...column.getHeaderProps(column.getSortByToggleProps())}>
+									{column.render("Header")}{" "}
+									<span>
+										{column.isSorted ? (column.isSortedDesc ? "⬇️" : "⬆️") : ""}
+									</span>
+								</th>
+							))}
 						</tr>
-					);
-				})}
-			</tbody>
-		</table>
+					))}
+				</thead>
+				<tbody {...getTableBodyProps()}>
+					{rows.map((row) => {
+						prepareRow(row);
+						return (
+							<tr {...row.getRowProps()}>
+								{row.cells.map((cell) => {
+									return (
+										<td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+									);
+								})}
+							</tr>
+						);
+					})}
+				</tbody>
+			</table>
+		</>
 	);
 }
 
