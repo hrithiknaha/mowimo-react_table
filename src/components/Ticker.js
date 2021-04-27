dc8b0474a783e5daffb92ad0cbfb153dca6ed454
Hrithik Naha <naha.hrithkk@gmail.com>
2021-04-27 19:19:58 +0530

    Add ticker component

--- src/components/Ticker.js
+++ src/components/Ticker.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
 
 function Ticker() {
-	return <div>Hi</div>;
+	const [data, setData] = useState({});
+	const [isLoading, setLoading] = useState(true);
+
+	const { ticker } = useParams();
+	console.log(data);
+	useEffect(() => {
+		async function fetchData() {
+			const response = await axios.get(
+				`https://mysql-test-2021.herokuapp.com/score/${ticker}`
+			);
+			setData(response.data[0]);
+			setLoading(false);
+		}
+
+		fetchData();
+	}, []);
+	return <>{!isLoading && <span>{data.sec_ticker}</span>}</>;
 }
 
 export default Ticker;
