de5bd6c6b3f6449485b31a7c05bd68fb59d584d8
Hrithik Naha <naha.hrithkk@gmail.com>
2021-04-28 14:46:23 +0530

    Fixed spellCheck and className

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import axios from "axios";
 
--- src/components/Filter.js
+++ src/components/Filter.js
@@ -3,14 +3,14 @@ import React from "react";
 //Just setting the filter as value and setFilter passed from parent as prop to onchange
 function Filter({ filter, setFilter }) {
 	return (
-		<div class="filter">
+		<div className="filter">
 			<input
 				className="filter-input"
 				type="text"
 				name="input-text"
 				id="input-text"
 				required
-				spellcheck="false"
+				spellCheck="false"
 				value={filter || ""}
 				onChange={(e) => setFilter(e.target.value)}
 				placeholder="Filter"
