453c774198bb32ed90cc14a90406f05915a4efb6
Hrithik Naha <naha.hrithkk@gmail.com>
2021-05-01 17:00:17 +0530

    removed unnecessary functions

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import axios from "axios";
 
 import Table from "./components/Table";
 import Ticker from "./components/Ticker";
