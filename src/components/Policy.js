e23258fa953cca92f0b13331ad44573e408f1a87
Hrithik Naha <naha.hrithkk@gmail.com>
2021-05-08 16:55:43 +0530

    add imprint and policy

---
+++ src/components/Policy.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+function Policy() {
+	return (
+		<div className="container policy">
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias ullam
+				deserunt possimus nesciunt minima illum sapiente earum doloremque
+				temporibus quidem.
+			</p>
+			<p>
+				Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et aliquid id
+				dignissimos repellendus molestias debitis!
+			</p>
+			<p>
+				Lorem ipsum dolor, sit amet consectetur adipisicing elit. Assumenda quia
+				asperiores ab repellendus vitae, labore explicabo? Aperiam, laborum
+				sequi? Dignissimos iure unde voluptatibus dolore nisi? Delectus facere
+				libero necessitatibus perferendis.
+			</p>
+		</div>
+	);
+}
+
+export default Policy;
