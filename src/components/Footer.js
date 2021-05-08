66735b504e04d2f6967635d947bedec9921c3470
Hrithik Naha <naha.hrithkk@gmail.com>
2021-05-08 16:55:32 +0530

    add footer styles and component

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Footer() {
+	return (
+		<div className="footer">
+			<div className="footer-links">
+				<p>Privacy Policy</p>
+				<p>|</p>
+				<p>Imprint</p>
+			</div>
+		</div>
+	);
+}
+
+export default Footer;
