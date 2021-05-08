4c418214ee8f13c76c6a5ecab0efaa3235cd3220
Hrithik Naha <naha.hrithkk@gmail.com>
2021-05-08 16:55:53 +0530

    add footer to app component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import Table from "./components/Table";
 import Ticker from "./components/Ticker";
+import Footer from "./components/Footer";
 
 import "../src/styles/styles.scss";
 
@@ -16,6 +17,7 @@ function App({ i18n }) {
 				</Route>
 				<Route path="/score/:ticker" exact component={Ticker} />
 			</Switch>
+			<Footer />
 		</Router>
 	);
 }
