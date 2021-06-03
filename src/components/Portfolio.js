932a571061ec6522a45fd028d48c971bc97083aa
Hrithik Naha <naha.hrithkk@gmail.com>
2021-06-03 22:20:08 +0530

    useeffect

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import { connect } from "react-redux";
 
 function Portfolio({ table }) {
+	useEffect(() => {
+		let portfolio = [];
+		table.portfolio.map((stock) => {
+			const stock_ticker = stock.split(",")[0];
+			const each_stock = table.rows.filter(
+				(row) => row.sec_ticker === stock_ticker
+			);
+			portfolio = [...portfolio, each_stock[0]];
+			console.log(portfolio);
+		});
+	}, []);
 	return (
 		<div className="portfolio">
 			<table className="sidebar-portfolio-table">
