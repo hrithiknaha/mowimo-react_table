e7eb21768fdfd69120abf1c97f73a65921228091
Hrithik Naha <naha.hrithkk@gmail.com>
2021-06-08 01:00:09 +0530

    bug fix as on refresh no data was showing

--- src/components/StockEarnings.js
+++ src/components/StockEarnings.js
@@ -6,8 +6,8 @@ import { earnings_column } from "../config/earnings";
 function StockEarnings() {
 	const [earnings, setEarnings] = useState([]);
 
-	const columns = useMemo(() => earnings_column, []);
-	const data = useMemo(() => earnings, []);
+	const columns = useMemo(() => earnings_column, [earnings_column]);
+	const data = useMemo(() => earnings, [earnings]);
 
 	useEffect(() => {
 		axios
@@ -28,27 +28,39 @@ function StockEarnings() {
 
 	return (
 		<table {...getTableProps()}>
+			{/* Maping any header groups first (Grouped Header). then mapping each
+				column inside of grouped header to get each individual columnsa and its
+				index. Printing the Header and the tooltip */}
 			<thead>
 				{headerGroups.map((headerGroup) => (
 					<tr {...headerGroup.getHeaderGroupProps()}>
-						{headerGroup.headers.map((column) => (
-							<th {...column.getHeaderProps(column.getSortByToggleProps())}>
-								{column.render("Header")}
-								<span>
-									{column.isSorted ? (column.isSortedDesc ? " 🔽" : " 🔼") : ""}
-								</span>
+						{headerGroup.headers.map((column, index) => (
+							<th
+								className="tooltip"
+								{...column.getHeaderProps(column.getSortByToggleProps())}
+							>
+								{column.render("Header")}{" "}
+								{headerGroup.headers[index].tipText && (
+									<span>{headerGroup.headers[index].tipText}</span>
+								)}
 							</th>
 						))}
 					</tr>
 				))}
 			</thead>
+			{/* Mapping page index with default size of 10, and then mapping each row
+				inside of each age to get all the rows. */}
 			<tbody {...getTableBodyProps()}>
 				{rows.map((row) => {
 					prepareRow(row);
 					return (
 						<tr {...row.getRowProps()}>
 							{row.cells.map((cell) => {
-								return <td {...cell.getCellProps()}>{cell.render("Cell")}</td>;
+								return (
+									<td className="row" {...cell.getCellProps()}>
+										{cell.render("Cell")}
+									</td>
+								);
 							})}
 						</tr>
 					);
